7dec1f1e15d5bfee56902b9a91dea3dad28563f4
Krzysztof Goliński <krzysztof@golinski.org>
2022-11-13 08:52:04 +0100

    tweaks

--- themes/essay/assets/js/responses.js
+++ themes/essay/assets/js/responses.js
@@ -12,20 +12,29 @@ document.addEventListener("DOMContentLoaded", function () {
   let wHeight = window.innerHeight
   let center = wHeight / 2
   let height = covers[0].getBoundingClientRect().height
-  let maxImgHeight = covers[0].querySelector('img').getBoundingClientRect().height
-  let minImgHeight = 200
+  let maxImgHeight = covers[0].getBoundingClientRect().height*0.5
+  let minImgHeight = covers[0].getBoundingClientRect().height/4
 
   //console.log('height:' + window.innerHeight)
   //console.log(center)
   //console.log(margins)
-  console.log('maxImgHeight:' + maxImgHeight)
+  //console.log('maxImgHeight:' + maxImgHeight)
+
+  covers.forEach(cover => {
+    let top = cover.getBoundingClientRect().top
+    let pos = top + height / 2
+    cover.style.opacity = clamp(map(pos, 0, center, 0, 1), 0, 1)
+    let image = cover.querySelector('img')
+    image.height = clamp(map(pos, wHeight, center, minImgHeight, maxImgHeight), minImgHeight, maxImgHeight)
+  })
+
   document.addEventListener("scroll", () => {
     let y = document.documentElement["scrollTop"] || document.body["scrollTop"];
     let total = (document.documentElement["scrollHeight"] || document.body["scrollHeight"]) - document.documentElement.clientHeight;
     // console.log(y/total)
 
-    console.log('top:' + covers[0].getBoundingClientRect().top)
-    console.log('height:' + covers[0].getBoundingClientRect().height)
+    //console.log('top:' + covers[0].getBoundingClientRect().top)
+    //console.log('height:' + covers[0].getBoundingClientRect().height)
     //console.log('offset:'+ covers[0].offsetTop)
 
     covers.forEach(cover => {
@@ -34,8 +43,6 @@ document.addEventListener("DOMContentLoaded", function () {
       cover.style.opacity = clamp(map(pos, 0, center, 0, 1), 0, 1)
       let image = cover.querySelector('img')
       image.height = clamp(map(pos, wHeight, center, minImgHeight,maxImgHeight), minImgHeight, maxImgHeight)
-
-
     })
   },
     { passive: true }
